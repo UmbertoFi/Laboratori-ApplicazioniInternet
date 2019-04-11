216292a6beef7a368a704ea302f8d3b98de53538
ShadiSmith01 <fierroumberto@gmail.com>
2019-04-11 18:12:08 +0200

    Rimosso Lab2

--- Lab2/src/main/java/it/polito/ai/lab2/demo/Service/FermataService.java
+++
@@ -1,8 +0,0 @@
-package it.polito.ai.lab2.demo.Service;
-
-import it.polito.ai.lab2.demo.Entity.Linea;
-
-public interface FermataService {
-
-    public void addFermate(Linea l);
-}
--- Lab2/src/main/java/it/polito/ai/lab2/demo/Service/PrenotazioneService.java
+++
@@ -1,8 +0,0 @@
-package it.polito.ai.lab2.demo.Service;
-
-import it.polito.ai.lab2.demo.Entity.Prenotazione;
-
-public interface PrenotazioneService {
-    public void addPrenotazione(Prenotazione p);
-}
-
