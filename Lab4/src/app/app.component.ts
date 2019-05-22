8bb9e571c15b13e457d2646ae9ed73874933a80c
barberinho <andrea.barbero@outlook.it>
2019-05-22 22:20:18 +0200

    Barbe: popolato json

--- Lab4/src/app/app.component.ts
+++ Lab4/src/app/app.component.ts
@@ -398,6 +398,313 @@ export class AppComponent implements OnInit {
           ]
         }
       ]
+    },
+    {
+      nome: '33',
+      corse: [
+        {
+          data: '2019/05/27',
+          tratte: [
+            {
+              verso: 'andata',
+              fermate: [{
+                nome: 'Porta Nuova',
+                ora: '08.05',
+                persone: [
+                  {nome: 'Antonino', selected: false},
+                  {nome: 'Paolo', selected: false},
+                  {nome: 'Umberto', selected: false}
+                ]
+              },
+                {
+                  nome: 'Vittorio_Emanuele_II',
+                  ora: '08.10',
+                  persone: [
+                    {nome: 'Christian', selected: false},
+                    {nome: 'Luca', selected: false},
+                    {nome: 'Andrea', selected: false},
+                    {nome: 'Lucia', selected: false},
+                    {nome: 'Valentina', selected: false},
+                  ]
+                },
+                {
+                  nome: 'Re Umberto',
+                  ora: '08.15',
+                  persone: [
+                    {nome: 'Francesco', selected: false},
+                    {nome: 'Salvatore', selected: false},
+                    {nome: 'Maria', selected: false},
+                    {nome: 'Marianna', selected: false}
+                  ]
+                },
+                {
+                  nome: 'Stati Uniti',
+                  ora: '08.20',
+                  persone: [
+                    {nome: 'Kevin', selected: false},
+                    {nome: 'Davide', selected: false},
+                    {nome: 'Matteo', selected: false},
+                    {nome: 'Gino', selected: false},
+                    {nome: 'Carla', selected: false}
+                  ]
+                },
+                {
+                  nome: 'Scuola',
+                  ora: '08.30',
+                  persone: []
+                }]
+            },
+            {
+              verso: 'ritorno',
+              fermate: [{
+                nome: 'Scuola',
+                ora: '13.35',
+                persone: []
+              },
+                {
+                  nome: 'Stati Uniti',
+                  ora: '13.40',
+                  persone: [
+                    {nome: 'Filippo', selected: false},
+                    {nome: 'Tommaso', selected: false},
+                    {nome: 'Lorenzo', selected: false},
+                    {nome: 'Tina', selected: false},
+                    {nome: 'Rebecca', selected: false}
+                  ]
+                },
+                {
+                  nome: 'Re Umberto',
+                  ora: '13.45',
+                  persone: [
+                    {nome: 'Dario', selected: false},
+                    {nome: 'Emanuele', selected: false},
+                    {nome: 'Valentina', selected: false}
+                  ]
+                },
+                {
+                  nome: 'Vittorio_Emanuele_II',
+                  ora: '13.50',
+                  persone: [
+                    {nome: 'Diana', selected: false},
+                    {nome: 'Sonia', selected: false},
+                    {nome: 'Veronica', selected: false}
+                  ]
+                },
+                {
+                  nome: 'Porta_Nuova',
+                  ora: '13.55',
+                  persone: [
+                    {nome: 'Maura', selected: false},
+                    {nome: 'Federico', selected: false},
+                    {nome: 'Giorgio', selected: false}
+                  ]
+                }]
+            }
+          ]
+        },
+        {
+          data: '2019/09/29',
+          tratte: [
+            {
+              verso: 'andata',
+              fermate: [{
+                nome: 'Porta Nuova',
+                ora: '08.05',
+                persone: [
+                  {nome: 'Silvia', selected: false},
+                  {nome: 'Paolo', selected: false},
+                  {nome: 'Ilaria', selected: false}
+                ]
+              },
+                {
+                  nome: 'Vittorio_Emanuele_II',
+                  ora: '08.10',
+                  persone: [
+                    {nome: 'Alessandro', selected: false},
+                    {nome: 'Luca', selected: false},
+                    {nome: 'Alessio', selected: false},
+                    {nome: 'Laura', selected: false},
+                    {nome: 'Valentina', selected: false},
+                  ]
+                },
+                {
+                  nome: 'Re Umberto',
+                  ora: '08.15',
+                  persone: [
+                    {nome: 'Francesca', selected: false},
+                    {nome: 'Davide', selected: false},
+                    {nome: 'Daniele', selected: false},
+                    {nome: 'Marianna', selected: false}
+                  ]
+                },
+                {
+                  nome: 'Stati Uniti',
+                  ora: '08.20',
+                  persone: [
+                    {nome: 'Kevin', selected: false},
+                    {nome: 'Davide', selected: false},
+                    {nome: 'Mattia', selected: false},
+                    {nome: 'Ginevra', selected: false},
+                    {nome: 'Gennaro', selected: false}
+                  ]
+                },
+                {
+                  nome: 'Scuola',
+                  ora: '08.30',
+                  persone: []
+                }]
+            },
+            {
+              verso: 'ritorno',
+              fermate: [{
+                nome: 'Scuola',
+                ora: '13.35',
+                persone: []
+              },
+                {
+                  nome: 'Stati Uniti',
+                  ora: '13.40',
+                  persone: [
+                    {nome: 'Filippo', selected: false},
+                    {nome: 'Giorgio', selected: false},
+                    {nome: 'Lorenza', selected: false},
+                    {nome: 'Angelica', selected: false},
+                  ]
+                },
+                {
+                  nome: 'Re Umberto',
+                  ora: '13.45',
+                  persone: [
+                    {nome: 'Martina', selected: false},
+                    {nome: 'Lidia', selected: false},
+                    {nome: 'Valentina', selected: false}
+                  ]
+                },
+                {
+                  nome: 'Vittorio_Emanuele_II',
+                  ora: '13.50',
+                  persone: [
+                    {nome: 'Debora', selected: false},
+                    {nome: 'Sara', selected: false},
+                    {nome: 'Camilla', selected: false}
+                  ]
+                },
+                {
+                  nome: 'Porta_Nuova',
+                  ora: '13.55',
+                  persone: [
+                    {nome: 'Alessio', selected: false},
+                    {nome: 'Francesco', selected: false},
+                    {nome: 'Giovanni', selected: false}
+                  ]
+                }]
+            }
+          ]
+        },
+        {
+          data: '2019/11/31',
+          tratte: [
+            {
+              verso: 'andata',
+              fermate: [{
+                nome: 'Porta Nuova',
+                ora: '08.05',
+                persone: [
+                  {nome: 'Stefano', selected: false},
+                  {nome: 'Luigi', selected: false},
+                  {nome: 'Umberto', selected: false},
+                  {nome: 'Luciano', selected: false}
+                ]
+              },
+                {
+                  nome: 'Vittorio_Emanuele_II',
+                  ora: '08.10',
+                  persone: [
+                    {nome: 'Leonardo', selected: false},
+                    {nome: 'Laura', selected: false},
+                    {nome: 'Alberto', selected: false},
+                    {nome: 'Lara', selected: false},
+                    {nome: 'Valerio', selected: false},
+                  ]
+                },
+                {
+                  nome: 'Re Umberto',
+                  ora: '08.15',
+                  persone: [
+                    {nome: 'Manuela', selected: false},
+                    {nome: 'Marco', selected: false},
+                    {nome: 'Sabrina', selected: false},
+                    {nome: 'Jacopo', selected: false}
+                  ]
+                },
+                {
+                  nome: 'Stati Uniti',
+                  ora: '08.20',
+                  persone: [
+                    {nome: 'Kevin', selected: false},
+                    {nome: 'Daniel', selected: false},
+                    {nome: 'Ilaria', selected: false},
+                    {nome: 'Cecilia', selected: false},
+                    {nome: 'Carla', selected: false}
+                  ]
+                },
+                {
+                  nome: 'Scuola',
+                  ora: '08.30',
+                  persone: []
+                }]
+            },
+            {
+              verso: 'ritorno',
+              fermate: [{
+                nome: 'Scuola',
+                ora: '13.35',
+                persone: []
+              },
+                {
+                  nome: 'Stati Uniti',
+                  ora: '13.40',
+                  persone: [
+                    {nome: 'Angelo', selected: false},
+                    {nome: 'Marina', selected: false},
+                    {nome: 'Valentino', selected: false},
+                    {nome: 'Antonio', selected: false}
+                  ]
+                },
+                {
+                  nome: 'Re Umberto',
+                  ora: '13.45',
+                  persone: [
+                    {nome: 'Davide', selected: false},
+                    {nome: 'Elena', selected: false},
+                    {nome: 'Vittoria', selected: false}
+                  ]
+                },
+                {
+                  nome: 'Vittorio_Emanuele_II',
+                  ora: '13.50',
+                  persone: [
+                    {nome: 'Diana', selected: false},
+                    {nome: 'Sonia', selected: false},
+                    {nome: 'Veronica', selected: false},
+                    {nome: 'Andrea', selected: false},
+                    {nome: 'Matteo', selected: false},
+                    {nome: 'Riccardo', selected: false}
+                  ]
+                },
+                {
+                  nome: 'Porta_Nuova',
+                  ora: '13.55',
+                  persone: [
+                    {nome: 'Anna', selected: false},
+                    {nome: 'Nicola', selected: false},
+                    {nome: 'Gino', selected: false}
+                  ]
+                }]
+            }
+          ]
+        }
+      ]
     }
   ];
 
